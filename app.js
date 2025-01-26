c7a6f8c0f60b2f4e818a851b3c98c34e156b8ba7
erenbolukoglu <eren87@gmail.com>
2025-01-26 22:14:57 +0300

    env

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config()
 
 var urlTicket = "https:/apiv2.bubilet.com.tr/api/v2/search/web/sayg%C4%B11";
 
 axios.get(urlTicket, {headers:{'user-agent': 'not axios'}}).then((res) => {
-    if(res.data.length>0) {
+    if(res.data.length>0 || process.env.SENDOK=='ok'){
         console.log("ok");
         axios.post('https://api.callmebot.com/whatsapp.php?phone=905057897922&text=saygı1&apikey=2888623');
     }
