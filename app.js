5d8d860499e4320ec07ed7b77a14185714187784
erenbolukoglu <eren87@gmail.com>
2025-01-27 01:19:16 +0300

    express

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const axios = require('axios');
 const dotenv = require('dotenv');
+const express = require('express');
+const app = express();
 dotenv.config()
 
 var urlTicket = "https:/apiv2.bubilet.com.tr/api/v2/search/web/sayg%C4%B11";
 
-axios.get(urlTicket, {headers:{'user-agent': 'not axios'}}).then((res) => {
+const sendSMS = () => {axios.get(urlTicket, {headers:{'user-agent': 'not axios'}}).then((res) => {
     if(res.data.length>0 || process.env.SENDOK=='ok'){
         console.log("ok");
         axios.post('https://api.callmebot.com/whatsapp.php?phone=905057897922&text=saygı1&apikey=2888623');
     }
-    else{
+    else if(process.env.SENDOK=='nok'){
         axios.post('https://api.callmebot.com/whatsapp.php?phone=905057897922&text=bilet yok&apikey=2888623');
         console.log("not ok");
     }
 }).catch((err) => {
     console.log(err);
-})
+})}
+
+
+app.get('/', function (req, res) {
+    res.send(200, { message: 'ok' });
+    sendSMS();
+});
+
+
+app.listen(3000, function () {
+    console.log('Sunucu çalışıyor...');
+  });
